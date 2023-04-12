77745f7bd3884b77e580d7d510a856b965ee6d43
hungban1995 <112172651+hungban1995@users.noreply.github.com>
2023-04-13 00:02:20 +0700

    Update cors.config.js

--- backend/src/configs/cors.config.js
+++ backend/src/configs/cors.config.js
@@ -1,19 +1,32 @@
-import cors from "cors";
-const corsConfig = (app) => {
-  const whitelist = [
-    "http://sport-shop-client-three.vercel.app",
-    "https://sport-shop-admin.vercel.app",
-  ];
+// import cors from "cors";
+// const corsConfig = (app) => {
+//   const whitelist = [
+//     "http://sport-shop-client-three.vercel.app",
+//     "https://sport-shop-admin.vercel.app",
+//   ];
 
+//   const corsOptionsDelegate = function (req, callback) {
+//     let corsOptions;
+//     if (whitelist.indexOf(req.header("Origin")) !== -1) {
+//       corsOptions = { origin: true }; // reflect (enable) the requested origin in the CORS response
+//     } else {
+//       corsOptions = { origin: false }; // disable CORS for this request
+//     }
+//     callback(null, corsOptions); // callback expects two parameters: error and options
+//   };
+//   app.use(cors(corsOptionsDelegate));
+// };
+// export default corsConfig;
+
+import cors from "cors";
+
+const corsConfig = (app) => {
   const corsOptionsDelegate = function (req, callback) {
-    let corsOptions;
-    if (whitelist.indexOf(req.header("Origin")) !== -1) {
-      corsOptions = { origin: true }; // reflect (enable) the requested origin in the CORS response
-    } else {
-      corsOptions = { origin: false }; // disable CORS for this request
-    }
-    callback(null, corsOptions); // callback expects two parameters: error and options
+    const corsOptions = { origin: true };
+    callback(null, corsOptions);
   };
   app.use(cors(corsOptionsDelegate));
 };
+
 export default corsConfig;
+
